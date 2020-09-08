e9cb792067ab60da0f1e7c83f53aa17e9366b938
turojosephkings <turojkings@gmail.com>
2020-09-08 18:11:37 -0400

    Amazon Clone 1st Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAS7Hj8CBkT1dcyZtNIlNOwmmCcKMGtNNE",
+    authDomain: "clone-70534.firebaseapp.com",
+    databaseURL: "https://clone-70534.firebaseio.com",
+    projectId: "clone-70534",
+    storageBucket: "clone-70534.appspot.com",
+    messagingSenderId: "319903783248",
+    appId: "1:319903783248:web:c7319a44b3ca35d86ff455"
+  };
